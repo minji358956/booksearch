086f012f570d31fb3ffbf2881025cd9844966458
minji358956 <minji358956@gmail.com>
2023-09-12 10:11:11 +0900

    first

---
+++ lib/book.dart
@@ -0,0 +1,44 @@
+class Book {
+  String id;
+  String title;
+  String subtitle;
+  String thumbnail; // 썸네일 이미지 링크
+  String previewLink; // ListTile 을 눌렀을 때 이동하는 링크
+  List authors;
+  String publishedDate;
+
+  //생성자
+  Book({
+    required this.id,
+    required this.title,
+    required this.subtitle,
+    required this.authors,
+    required this.publishedDate,
+    required this.thumbnail,
+    required this.previewLink,
+  });
+
+  Map toJson() {
+    return {
+      "id": id,
+      "title": title,
+      "subtitle": subtitle,
+      "authors": authors,
+      "publishedDate": publishedDate,
+      "thumbnail": thumbnail,
+      "previewLink": previewLink,
+    };
+  }
+
+  factory Book.fromJson(json) {
+    return Book(
+      id: json['id'],
+      title: json['title'],
+      subtitle: json['subtitle'],
+      authors: json['authors'],
+      publishedDate: json['publishedDate'],
+      thumbnail: json['thumbnail'],
+      previewLink: json['previewLink'],
+    );
+  }
+}
